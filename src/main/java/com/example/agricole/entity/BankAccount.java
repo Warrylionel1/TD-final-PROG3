994f75cc7b00c5578cd289f37d5a8c701013f579
Warry <hei.warry@gmail.com>
2026-04-22 16:35:05 +0300

    feat: add BankAccount entity for bank financial accounts

---
+++ src/main/java/com/example/agricole/entity/BankAccount.java
@@ -0,0 +1,16 @@
+package com.example.agricole.entity;
+
+import com.example.agricole.enums.Bank;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class BankAccount extends FinancialAccount {
+    private String holderName;
+    private Bank bankName;
+    private int bankCode;
+    private int bankBranchCode;
+    private int bankAccountNumber;
+    private int bankAccountKey;
+}
