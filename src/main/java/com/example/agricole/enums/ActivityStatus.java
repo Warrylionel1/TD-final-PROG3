d131db9907a095bb5864c51aec5073ea3ac4d8a1
Warry <hei.warry@gmail.com>
2026-04-22 16:07:18 +0300

    feat(enums): add ActivityStatus, Bank, Frequency, MobileBankingService,PaymentMode enums

---
+++ src/main/java/com/example/agricole/enums/ActivityStatus.java
@@ -0,0 +1,5 @@
+package com.example.agricole.enums;
+
+public enum ActivityStatus {
+    ACTIVE, INACTIVE
+}
---
+++ src/main/java/com/example/agricole/enums/Frequency.java
@@ -0,0 +1,5 @@
+package com.example.agricole.enums;
+
+public enum Frequency {
+    WEEKLY, MONTHLY, ANNUALLY, PUNCTUALLY
+}
